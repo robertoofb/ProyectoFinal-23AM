5356eaddee2f8a5335d3cc019557a4b0df51fc26
robertoofb <133799782+robertoofb@users.noreply.github.com>
2023-07-17 00:53:40 -0500

    Agregar archivos de proyecto.

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_23AM.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal_23AM.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            //Cadena de conexion
+            options.UseMySQL("server=localhost; database=proyectofinal-fierro; user=root; password=");
+            //Si hay error con la mmigracion prueba esta
+            //optionsBuilder.UseMySQL("Server=localhost;port=3306;User ID=root; Database=Empleados23BM");
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Rol> Roles { get; set; }
+
+    }
+}  
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ProyectoFinal_23AM.Vistas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProyectoFinal_23AM
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            Sistema sistema = new Sistema();
+            sistema.Show();
+            Close();
+        }
+    }
+}
